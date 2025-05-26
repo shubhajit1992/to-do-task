8a066e092ba6ba483ee41949efa4fcdeaed55fb6
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2025-05-26 15:02:09 +0530

    Replace @MockBean(deprecated) with @MockitoBean

--- src/test/java/com/shubhajit/todotask/controller/TaskControllerTest.java
+++ src/test/java/com/shubhajit/todotask/controller/TaskControllerTest.java
@@ -16,8 +16,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -28,7 +28,7 @@ class TaskControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TaskService taskService;
 
     @Autowired
